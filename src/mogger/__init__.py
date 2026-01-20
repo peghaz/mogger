b8c5348f6b08d9f2752dfde63821afe12bec7eca
Mehdi <mehdiyamin@gmail.com>
2026-01-20 18:18:34 +0400

    Update versioning to use pyproject.toml and add toml as a dependency
    - Modified __init__.py to load version from pyproject.toml instead of hardcoding.
    - Updated version in uv.lock from 0.1.1 to 0.2.1.
    - Added toml as a development dependency in uv.lock.
    - Included toml package details in uv.lock.

--- src/mogger/__init__.py
+++ src/mogger/__init__.py
@@ -1,5 +1,8 @@
+import toml
 from .core import Mogger
 from .loki import LokiConfig, LokiLogger
 
-__version__ = "0.1.1"
+pyproject_conf = toml.load("pyproject.toml")
+__version__ = pyproject_conf["project"]["version"]
+
 __all__ = ["Mogger", "LokiConfig", "LokiLogger"]
